6ab69a63317ed3db54e21615471697898960d9ff
jaredbeck12 <jaredbeck12@gmail.com>
2017-03-28 21:09:50 -0700

    This is my file.

---
+++ myRfile.R
@@ -0,0 +1,8 @@
+## This is an R file 
+
+plot(1:10)
+
+
+## Apparently there are terms like "repo," "commit," and "push" 
+## "Repo" means to create a repository
+## "Commit," which you have to do before you push 
